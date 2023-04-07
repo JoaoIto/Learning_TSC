032dce965c47150b5beb6812029b5fdccf59a536
João <joaovictorpfr@gmail.com>
2023-04-07 14:28:15 -0300

    no tests

--- src/Base/types/index.ts
+++ src/Base/types/index.ts
@@ -40,6 +40,35 @@ const user: User = new User("Joao", "joaovictorpfr@gmail.com", 17, true);
 
 console.log(user);
 console.log(typeof user.name, typeof user.age, typeof user.email, typeof user.isCoding)
+
+// Tests (doesnt needs! doesnt needs in typescript!):
+
+describe('User', () => {
+  it('should create a new user instance with correct properties', () => {
+    const user: User = new User("Joao", "joaovictorpfr@gmail.com", 17, true);
+
+    expect(user).toBeDefined();
+    expect(user.name).toEqual('Joao');
+    expect(user.email).toEqual('joaovictorpfr@gmail.com');
+    expect(user.age).toEqual(17);
+    expect(user.isCoding).toBeTruthy();
+  });
+
+  it('should ensure property types match', () => {
+    const user: User = new User("Joao", "joaovictorpfr@gmail.com", 17, true);
+
+    expect(typeof user.name).toBe('string');
+    expect(typeof user.age).toBe('number');
+    expect(typeof user.email).toBe('string');
+    expect(typeof user.isCoding).toBe('boolean');
+  });
+});
+
+
+
+function expect(name: string) {
+    throw new Error("Function not implemented.");
+}
 // Result in console: 
 
 /* 
